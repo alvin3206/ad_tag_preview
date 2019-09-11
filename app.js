57ba81348f2720d77cd1233bfea2cd3ddf137bed
alvin3206 <alvin3206@gmail.com>
2019-09-11 12:18:58 +0800

    path revised

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const scrape = require('website-scraper');
+const fs = require('fs');
+
 
 const app = express();
 
@@ -15,26 +17,62 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get("/", function(req,res) {
-  res.render("list",{prevSite:""});
+  // 避免空白
+  res.render("list",{prevSite:"",preSiteUrl:""});
   // res.sendFile(__dirname + "/index.html");
+  console.log(__dirname);
 
 });
 
 app.post("/",function(req,res) {
-  var firstName = req.body.siteUrl;
+  var site = req.body.siteUrl;
   const dateTime = Date.now();
   const options = {
-    urls: [firstName],
+    urls: [site],
     directory: __dirname + '/sites/'+ dateTime ,
     subdirectories: [
       {directory: 'public', extensions: ['.jpg', '.png', '.svg', '.js', '.css', '.gif']},
     ],
   };
   const result = scrape(options);
-  res.render("list",{prevSite: __dirname + "/" + dateTime + "/index.html"});
+
+  fs.readFile(path.join(__dirname, './json/hp_mph.json'), 'utf8', function (err, data) {
+      if (err) throw err;
+      let list = JSON.parse(data);
+      // list.forEach((item,index)=>{
+      //     let value = item.properties;
+      //     let result = {};
+      //     result.ID = index + 1;
+      //     result.TYPE = value.FLAG_A;
+      //     result.X = value.X;
+      //     result.Y = value.Y;
+      //     newList.push(result);
+      // })
+
+      for (let i = 0; i < list.length; i++) {
+          let result = {};
+          let value = list[i].properties;
+          result.ID = i + 1;
+          result.TYPE = value.FLAG_A;
+          result.X = value.X;
+          result.Y = value.Y;
+          newList.push(result);
+      }
+
+      let newContent = JSON.stringify(newList, null, 4);
+      fs.writeFile('result.json', newContent, 'utf8', (err) => {
+          if (err) throw err;
+          console.log('success done');
+      });
+  });
+
+  res.render("list",{prevSite: "Mockup site", preSiteUrl: "/" + dateTime + "/index.html"});
+
+  // res.render("list",{prevSite: __dirname + "/site/" + dateTime + "/index.html", preSiteUrl: "/" + dateTime + "/index.html"});
 });
 
 app.get("/:customSite", function(req,res) {
+  // res.render(__dirname + '/sites/');
   console.log(req.params.customSite);
 });
 
