48cd82ef30f38cdb3bd4f8d4441970d3eae99f34
alvin3206 <alvin3206@gmail.com>
2019-09-16 18:57:41 +0800

    bubble position & size fixed

--- sites/1568630623985/public/index_append.js
+++ sites/1568630623985/public/index_append.js
@@ -61,8 +61,7 @@ document.querySelectorAll(".bubble, .ui-draggable ,.ui-draggable-handle")[0].add
   if (!($( "[id*='div-gpt']" ).hasClass("gpt_highlight"))) {
     $("[id*='div-gpt']").addClass("gpt_highlight");
   }
-  for (var i = 0 ; i < gptNumber ; i++) {
-    document.querySelectorAll("[id*='div-gpt']")[i].textContent = "DFP 廣告空間 "+ i;
-  }
+
+  $("[id*='div-gpt']").text("DFP 廣告空間");
 
 });
