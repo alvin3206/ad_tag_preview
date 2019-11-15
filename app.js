b5f18b2007f6b5360960cd92561cedc72d4498db
alvin3206 <alvin3206@gmail.com>
2019-11-15 09:25:25 +0800

    req.body.siteUrl encoded

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res) {
   // req.setTimeout(500000);
-  var site = req.body.siteUrl;
+  var site = encodeURI(req.body.siteUrl);
   const dateTime = Date.now();
   console.log("/" + dateTime + "/index.html");
   const options = {
