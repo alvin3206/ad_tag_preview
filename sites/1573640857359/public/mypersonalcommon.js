2d8a7839831732f95fb7addf95444619199e1595
alvin3206 <alvin3206@gmail.com>
2019-11-28 15:25:08 +0800

    update .gitignore

--- sites/1573640857359/public/mypersonalcommon.js
+++
@@ -1,9 +0,0 @@
-// var sso_url = "https://sso.sinchew.com.my:1443/SSOv2/api/user";
-// var sso_app = "https://sso.sinchew.com.my:1443/sso-app/api";
-// var xyUrl = "https://www.sinchew.com.my/app_if";
-
-var mysso_url = "https://sso.sinchew.com.my:1443/SSOv2/api/user";
-var mysso_app = "https://sso.sinchew.com.my:1445/sso-app/api";
-var myxyUrl = "https://www.sinchew.com.my/app_if";
-
-// var myxy_app = "https://www.sinchew.com.my/app_if/amuc/api/member/";
