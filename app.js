400b764229f5fc42dadec60b9785eea061a37ba1
Alvin Chen <alvin@tenmax.io>
2020-07-29 09:39:57 +0800

    add ga tag

--- app.js
+++ app.js
@@ -5,21 +5,16 @@ const bodyParser = require("body-parser");
 const scrape = require('website-scraper');
 const fs = require('fs');
 
-
 const app = express();
+const gaTag = "<script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-132057673-2');</script></head>";
 const bubbleOutLink = '<link rel="stylesheet" href="public/styles_append.css"></head><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script><script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>';
 const bubbleStyleString = '<div class="chat_container" style="visibility:hidden;z-index:100000">  <div class="bubble" style="visibility:visible"></div>  <div class="chat" style="visibility:visible">    <ul class="chat-thread">      <li><h5>          How many DFP Ad spaces on this page:          <span class="badge badge-pill badge-warning">9</span>        </h5></li>      <li><p>Replace DFP ad space:</p><select id="dfpDrop">     <!-- <option value="" selected disabled hidden>Choose DFP space</option> -->        </select></li><li><p>Ad Space Size</p><p id="spaceSize"></p></li>    <li><button id="confirmMapping" type="button" class="btn btn-primary">Replace with your tag</button></li>     </ul>  </div></div></body><script src="public/index_append.js" charset="utf-8"></script>';
-
-
-
 
 // app.use(express.static(__dirname + '/sites'));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-
-
 
 app.get("/", function(req, res) {
   app.use(express.static(__dirname + '/sites'));
@@ -39,10 +34,9 @@ app.post("/", function(req, res) {
   // req.setTimeout(500000);
   var site = encodeURI(req.body.siteUrl);
   var adTag = encodeURI(req.body.insertTag);
-  console.log(adTag);
+  // console.log(adTag);
   const dateTime = Date.now();
-  console.log("/" + dateTime + "/index.html");
-
+  // console.log("/" + dateTime + "/index.html");
   // class MyPlugin {
   //   apply(registerAction) {
   //     registerAction('afterFinish', async () => {});
@@ -57,29 +51,39 @@ app.post("/", function(req, res) {
       // plugins: [new MyPlugin()]
     }, ],
   };
-  console.log("post");
-
-
+  console.log("route:/ method:post");
 
   scrape(options).then((result) => {
     var styleResult;
     var jsReplace;
     var linkResult;
-    console.log("scrape");
-    console.log(req.body.disableSelect);
+    var gaReplace;
+    console.log("scrape end");
+    // console.log(req.body.disableSelect);
+    fs.readFile(__dirname + '/sites/' + dateTime + '/index.html', 'utf8', function(err, data) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("GA tag insert process begin");
+      gaReplace = data.replace('</body>', gaTag);
+      fs.writeFile(__dirname + '/sites/' + dateTime + '/index.html', gaReplace, 'utf8', function(err) {
+        if (err) return console.log(err);
+        console.log("GA tag insert process end");
+      });
+    });
 
     if (req.body.disableSelect !== "true") {
       fs.readFile(__dirname + '/sites/' + dateTime + '/index.html', 'utf8', function(err, data) {
         if (err) {
           return console.log(err);
         }
-        console.log("process1");
+        console.log("bubble replacement begin");
 
         styleResult = data.replace('</body>', bubbleStyleString);
         styleResult = styleResult.replace('</head>', bubbleOutLink);
         fs.writeFile(__dirname + '/sites/' + dateTime + '/index.html', styleResult, 'utf8', function(err) {
           if (err) return console.log(err);
-          console.log("process2");
+          console.log("bubble replacement end");
         });
       });
       fs.copyFile(__dirname + '/styles_append.css', __dirname + '/sites/' + dateTime + '/public/' + 'styles_append.css', (err) => {
@@ -101,13 +105,13 @@ app.post("/", function(req, res) {
         if (err) {
           return console.log(err);
         }
-        console.log("process3");
+        console.log("append.html (iframe, js) modification begin");
 
         jsReplace = data.replace('</body>', '</body><script type="text/javascript">' + decodeURI(adTag) + '</script>');
 
         fs.writeFile(__dirname + '/sites/' + dateTime + '/public/' + 'append.html', jsReplace, 'utf8', function(err) {
           if (err) return console.log(err);
-          console.log("process4");
+          console.log("append.html (iframe, js) modification end");
         });
       });
     } else if (req.body.codeFormat === "html") {
@@ -115,13 +119,13 @@ app.post("/", function(req, res) {
         if (err) {
           return console.log(err);
         }
-        console.log("process3");
+        console.log("append.html (iframe, html) modification begin");
 
         jsReplace = data.replace('<body>', '<body>' + decodeURI(adTag));
 
         fs.writeFile(__dirname + '/sites/' + dateTime + '/public/' + 'append.html', jsReplace, 'utf8', function(err) {
           if (err) return console.log(err);
-          console.log("process4");
+          console.log("append.html (iframe, html) modification end");
         });
       });
     }
@@ -147,7 +151,6 @@ app.get("/:siteId/", function(req, res) {
     }
   }
 });
-
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("server started running on port 3000!");
