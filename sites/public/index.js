edf1362428442a29a6d1b1e7e75e09ceb3599459
Alvin Chen <alvin@tenmax.io>
2020-08-05 18:45:41 +0800

    add helper toast add only show on the first time

--- sites/public/index.js
+++ sites/public/index.js
@@ -1,5 +1,19 @@
 console.log("index.js loaded");
 
+var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+window.onload = function() {
+  $('.toast').toast('show');
+  if (w < 768) {
+    $('.toast-container').removeClass('toast-position');
+  }
+  if (!$("#mock").hasClass('disabled')) {
+    $('.alert').alert('close');
+  }
+};
+
+
+
 document.querySelector("#disableSelectOption").addEventListener("change", function() {
   if ($(".disableLink").attr('disabled')) {
     $(".disableLink").removeAttr('disabled');
@@ -8,10 +22,15 @@ document.querySelector("#disableSelectOption").addEventListener("change", functi
   }
 });
 
-
 document.querySelector("#start").addEventListener("click", function() {
   console.log("test");
   if (!$("#mock").hasClass('disabled')) {
     $("#mock").addClass('disabled');
   }
+
+});
+
+
+document.querySelector(".tagForm").addEventListener("submit", function() {
+  $("#mock").html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>Loading...');
 });
